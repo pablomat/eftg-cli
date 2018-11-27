71a452dfa7f677e8034fa9efaf4bb880cb101a33
Pablo Staiano <1355981+pablomat@users.noreply.github.com>
2018-11-27 10:33:49 +0100

    Added bash completion for eftg-cli

---
+++ scripts/eftg-completion.bash
@@ -0,0 +1,7 @@
+#/usr/bin/env bash
+
+_eftg_completion() {
+    COMPREPLY=($(compgen -W "dlblocks install_docker install_dependencies install start stop status restart wallet remote_wallet enter logs cleanup optimize" "${COMP_WORDS[1]}"))
+}
+
+complete -F _eftg_completion eftg-cli
