78b4e248b8c0564f44b8d5713be879d66060af50
Pablo Staiano <1355981+pablomat@users.noreply.github.com>
2019-01-14 22:28:12 +0100

    Modified bootstrap script to display less content

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,11 @@ set -o errexit # exit on errors
 set -o nounset # exit on use of uninitialized variable
 set -o errtrace # inherits trap on ERR in function and subshell
 
+#RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+BLUE="$(tput setaf 4)"
+RESET="$(tput sgr0)"
+
 install_dependencies() {
     set +u
     local count=()
@@ -39,22 +44,31 @@ install_dependencies() {
 	fi
     done
 
+    printf "\n%s" "${BLUE}Checking software dependencies.${RESET}" "${BLUE}===============================${RESET}" "" ""
+
     if [[ ${#count[@]} -ne 0 ]]; then
         echo "In order to run eftg-cli, the following packages need to be installed : ${count[*]}"
         while true; do
             read -r -p "Do you wish to install these packages? (yes/no) " yn
+            echo
             case $yn in
                 [Yy]* )
-                        sudo apt update;
-                        sudo apt install "${count[@]}";
-                        if ! pip3 show beem &>/dev/null; then { pip3 install -U beem==0.20.9; } fi;
-                        break;;
+                    if [[ -e /etc/apt/sources.list ]]; then
+                        if ! /bin/grep -q universe /etc/apt/sources.list; then { /usr/bin/sudo /usr/bin/add-apt-repository universe &>/dev/null; } fi
+                    else
+                        echo "/etc/apt/sources.list doesn't exist"
+                        exit 1
+                    fi
+                    sudo apt -qq update &>/dev/null;
+                    sudo apt -qq install -y "${count[@]}";
+                    if ! pip3 show beem &>/dev/null; then { pip3 -q install -U beem==0.20.9; } fi;
+                    break;;
                 [Nn]* ) exit;;
                 * ) echo "Please answer yes or no.";;
             esac
         done
 	else
-	    if ! pip3 show beem &>/dev/null; then { pip3 install -U beem==0.20.9; } fi
+	    if ! pip3 show beem &>/dev/null; then { pip3 -q install -U beem==0.20.9; } else { printf "%s\n" "${GREEN}All pre-requisites are already installed${RESET}"; } fi
     fi
     set -u
 }
@@ -68,7 +82,7 @@ if [[ -d "${PD}/eftg-cli" ]]; then
     hash="$(/usr/bin/git rev-list --parents HEAD | /usr/bin/tail -1)"
     if [[ x"${hash}" != "x9c035091ce1249666ec08555a122b96414e679b8" ]]; then { echo "Repository in ${PD}/eftg-cli doesn't match github.com/pablomat/eftg-cli"; exit 1; } fi
 else
-	if ! /usr/bin/git clone https://github.com/pablomat/eftg-cli.git; then { echo "Critical error"; exit 1; } fi
+	if ! /usr/bin/git clone --quiet https://github.com/pablomat/eftg-cli.git; then { echo "Critical error"; exit 1; } fi
 fi
 if ! cd "${PD}/eftg-cli"; then { echo "Cannot cd to ${PD}/eftg-cli"; exit 1; } fi
 "${PD}"/eftg-cli/eftg-cli.sh setup
